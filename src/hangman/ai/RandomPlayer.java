83ea45ee112dfe837b36abeb16102ea0c579529b
jssedn <j.e.rainey@gmail.com>
2013-05-25 14:10:31 -0400

    Create RandomPlayer.java

---
+++ src/hangman/ai/RandomPlayer.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class RandomPlayer 
+{
+  private String word;
+	private char[] guessed;
+	private char[] guessable;
+	int count;
+	
+	public void readLetters() 
+	{
+		Scanner sc = new Scanner("letters.txt");
+		int count = 0;
+		while (sc.hasNextLine()) 
+		{
+			String s = sc.nextLine();
+			guessable[count] = s.charAt(0);
+			count++;
+		}	
+	}
+
+	public char randomPlayer()
+	{
+		while (true)
+		{
+			Random rand = new Random();
+			int r = rand.nextInt(word.length());
+			
+			boolean alreadyGuessed = false;
+			for (int i = 0; i < guessed.length; i++) 
+			{
+				if (guessable[i] == guessed[i]) 
+				{
+					alreadyGuessed = true;
+					break;
+				}
+			}
+			
+			if (!alreadyGuessed) 
+			{
+				guessed[count] = guessable[r];
+				count++;
+				return guessable[r];
+			}
+		}
+
+	}
+}
