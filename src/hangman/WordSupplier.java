630f5902f19f2fef2834dfee4a379f371b37ff71
Carlos Cuesta <carloscuesta@gmail.com>
2013-05-25 16:49:22 -0600

    Update WordSupplier.java
    Adds random word logic to this class. Uses file name that is passed to it from the HangmanGame.java file via a command line arguement

--- src/hangman/WordSupplier.java
+++ src/hangman/WordSupplier.java
@@ -1,13 +1,44 @@
 package hangman;
 
-/**
- * Indicates ability to supply a "guessable" word for the hangman game.
- * 
- * @author Vera Coberley
- * May 24, 2013
- */
-public interface WordSupplier {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class WordSupplier {
 	
-	public String nextWord();
+	public static String secretWord(String filename) throws IOException {
+		String randomWord = null;
+		int count = 0;
+		int i = 0;
+		String [] wordList = new String[50000];
+        
+        try {
+        	FileInputStream fstream = new FileInputStream(filename);
+        	DataInputStream in = new DataInputStream(fstream);
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	String strLine;
+
+        while ((strLine = br.readLine()) != null)  
+        {
+        	wordList[i] = strLine;
+        	i++;
+        	count++;
+        }
+        
+        in.close();
+    	Random rand = new Random();
+    	randomWord = wordList[rand.nextInt(count)];
+    
+        }
+        catch (FileNotFoundException e)
+        {
+        	System.out.println("File not found!");
+        }
+        return randomWord;
+	}
 
 }
